0e439e7202f07ae7bb3d222631a581d66ef0a054
MTPE <mtpe2301@gmail.com>
2025-02-21 15:52:22 +0700

    Xóa bí mật

--- Lab2_Flask/config.py
+++ Lab2_Flask/config.py
@@ -1,5 +1,5 @@
 # Cấu hình API Key từ Hugging Face
-HUGGING_FACE_API_KEY = "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"
+HUGGING_FACE_API_KEY = " "
 # Mô hình cho từng ngôn ngữ
 HUGGING_FACE_MODELS = {
     "en": "distilbert-base-uncased-finetuned-sst-2-english",
